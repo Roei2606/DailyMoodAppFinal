63935b32bb9cf4ab5261c1c471ba65843d243b33
Roei2606 <Roei2606@gmail.com>
2025-07-31 16:46:06 +0300

    Initial Commit

---
+++ DailyMoodAppFinalTests/DailyMoodAppFinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMoodAppFinalTests.swift
+//  DailyMoodAppFinalTests
+//
+//  Created by Roei Hakmon on 31/07/2025.
+//
+
+import Testing
+@testable import DailyMoodAppFinal
+
+struct DailyMoodAppFinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
